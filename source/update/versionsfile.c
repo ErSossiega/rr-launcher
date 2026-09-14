2a4d919c517a2f831fb9a6a279724e9549b5f3ac
VanzaKartWiiTeam <andrea.sinesi20@gmail.com>
2026-09-14 20:01:38 +0200

    Fetch updates from vanzakart.net
    api.vanzakart.net is proxied by Cloudflare, which does not forward port 8000, so
    the Wii could never reach it. vanzakart.net answers on 8000 over IPv4.

--- source/update/versionsfile.c
+++ source/update/versionsfile.c
@@ -26,8 +26,8 @@
 #include "../util.h"
 #include "versionsfile.h"
 
-#define _RRC_VERSIONSFILE_URL "http://api.vanzakart.net:8000/VanzaKart/VanzaKartVersion.txt"
-#define _RRC_VERSIONS_FILE_REMOVED_URL "http://api.vanzakart.net:8000/VanzaKart/VanzaKartDelete.txt"
+#define _RRC_VERSIONSFILE_URL "http://vanzakart.net:8000/VanzaKart/VanzaKartVersion.txt"
+#define _RRC_VERSIONS_FILE_REMOVED_URL "http://vanzakart.net:8000/VanzaKart/VanzaKartDelete.txt"
 // max array size
 #define _RRC_SPLIT_LIM 4096
 
